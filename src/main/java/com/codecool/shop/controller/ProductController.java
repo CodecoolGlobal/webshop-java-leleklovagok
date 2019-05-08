71e382adeebdb2e77c4213fb9688db52d8f7d314
norbi <sznorbert9414@gmail.com>
2019-05-08 14:39:04 +0200

    Checkout page

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -53,9 +53,9 @@ public class ProductController extends HttpServlet {
         } else {
             context.setVariable("products", productDataStore.getBy(supplierDataStore.find(Integer.parseInt(id))));
         }
-        context.setVariable("category", productCategoryDataStore.find(1));
-        context.setVariable("categories", productCategoryDataStore.getAll());
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+        //context.setVariable("category", productCategoryDataStore.find(1));
+        //context.setVariable("categories", productCategoryDataStore.getAll());
+        //context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         context.setVariable("quantity", shoppingCartStore.getSize());
         engine.process("product/index.html", context, resp.getWriter());
 
