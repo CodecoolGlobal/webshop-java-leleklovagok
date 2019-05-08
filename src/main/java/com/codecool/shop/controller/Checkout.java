d7c62dd243cbafb6bd9d818b62fbedd9e978dec2
norbi <sznorbert9414@gmail.com>
2019-05-08 15:02:46 +0200

    Checkout page messy

--- src/main/java/com/codecool/shop/controller/Checkout.java
+++ src/main/java/com/codecool/shop/controller/Checkout.java
@@ -1,4 +1,6 @@
-package com.codecool.shop.controller;
+/*package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -6,12 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 @WebServlet(urlPatterns = {"/checkout"})
 public class Checkout extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        engine.process("product/checkout.html", context, response.getWriter());
     }
 }
+*/
